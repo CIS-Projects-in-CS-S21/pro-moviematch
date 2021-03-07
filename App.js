9acf7380dcad0a34951a029b5eb3a3c9ffcee9c7
Kyle Skelly <kyle.skelly32@gmail.com>
2021-03-06 19:26:03 -0500

    Updated application to include a basic homepage with the logo on the header

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 100, height: 100 }}
+      source={require('./resources/MovieMatchLogo.png')}
+    />
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerTitle: props => <LogoTitle {...props} /> }}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
