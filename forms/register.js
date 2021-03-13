715f5a174fbc012e1138c9b9255ce7519bcc591e
David Lee <tuc56947@temple.edu>
2021-03-13 11:34:24 -0500

    MOV-70 Added register frontend

---
+++ forms/register.js
@@ -0,0 +1,101 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Text, View, Image, TextInput, Button, TouchableOpacity, StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default function RegisterScreen({ navigation }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+  
+    return (
+        <View style={styles.container}>
+          <Image style={styles.image} source={require('../resources/MovieMatchLogo.png')} />
+     
+          <StatusBar style="auto" />
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Email"
+              placeholderTextColor="#003f5c"
+              onChangeText={(email) => setEmail(email)}
+            />
+          </View>
+     
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Password"
+              placeholderTextColor="#003f5c"
+              secureTextEntry={true}
+              onChangeText={(password) => setPassword(password)}
+            />
+          </View>
+
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="First Name"
+              placeholderTextColor="#003f5c"
+              onChangeText={(firstName) => setFirstName(firstName)}
+            />
+          </View>
+     
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Last Name"
+              placeholderTextColor="#003f5c"
+              secureTextEntry={true}
+              onChangeText={(lastName) => setLastName(lastName)}
+            />
+          </View>
+
+          <TouchableOpacity style={styles.register_button}>
+          <Text style={styles.loginText}>REGISTER</Text>
+          </TouchableOpacity>
+       </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+   
+    image: {
+      marginBottom: 40,
+    },
+   
+    inputView: {
+      backgroundColor: "#FFC0CB",
+      borderRadius: 30,
+      width: "80%",
+      height: 45,
+      marginBottom: 20,
+   
+      alignItems: "center",
+    },
+   
+    TextInput: {
+      height: 50,
+      flex: 1,
+      padding: 10,
+      marginLeft: 20,
+    },
+
+    register_button: {
+      width: "80%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 20,
+      backgroundColor: "#99ccff",
+    },
+  });
