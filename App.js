eb689c50c3b6bc4ea079578ba987b465cf9e9212
Kyle Skelly <kyle.skelly32@gmail.com>
2021-03-15 23:59:19 -0400

    Navigation bar updated/logout button functional

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { Text, View, SafeAreaView, Image, TextInput, Button, TouchableOpacity, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from './forms/login.js';
 import RegisterScreen from './forms/register.js';
 import Swiper from 'react-native-deck-swiper'
 import { Card } from './components/Cards.js'
 import { SwipeableMovies } from './constants/Movies.js'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function HomeScreen({ navigation }) {
   return (
@@ -15,12 +17,61 @@ function HomeScreen({ navigation }) {
           cards={SwipeableMovies}
           renderCard={Card}
           infinite // keep looping cards infinitely
-          verticalSwipe={false}
+          verticalSwipe={false} //disables vertical swipe
           backgroundColor="white"
           cardHorizontalMargin={0}
           stackSize={2} // number of cards shown in background
           />
     </SafeAreaView>        
+  );
+}
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'ios-home'
+              : 'ios-home-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'ios-settings-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#FF1493',
+        inactiveTintColor: 'gray',
+    }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+
+function SettingsScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity style={styles.logout_button} onPress={() =>
+        navigation.reset({
+          index: 0,
+          routes: [
+           {
+            name: 'Login',
+            params: { someParam: 'Param1'},
+           },
+          ],
+        })
+      }>
+        <Text style={styles.loginText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -51,26 +102,27 @@ const styles = StyleSheet.create({
 })
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-      ></Stack.Screen>
-      <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerTitle: props => <LogoTitle {...props} /> }}
-      />
-      <Stack.Screen
-        name="Register"
-        component={RegisterScreen}
-      ></Stack.Screen>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+        ></Stack.Screen>
+        <Stack.Screen
+            name="Home"
+            component={HomeTabs}
+            options={{ headerTitle: props => <LogoTitle {...props} /> }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+        ></Stack.Screen>
       </Stack.Navigator>
-    </NavigationContainer>
+  </NavigationContainer>
   );
 }
 
