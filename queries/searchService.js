741deba87aedc4d18180e6fa540f254b1a0d4f19
X'Zaiver Wolfinbarger <59936423+Wolfinbarger@users.noreply.github.com>
2021-04-01 14:47:51 -0500

    Connected API to search content
    Uses Utelly that can fetch information base on tmdb id

--- queries/searchService.js
+++ queries/searchService.js
@@ -1,22 +1,18 @@
-var unirest = require("unirest");
+const request = require('request');
 
-var req = unirest("GET", "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup");
+const options = {
+  method: 'GET',
+  url: 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup',
+  qs: {source_id: 'tt3398228', source: 'imdb', country: 'us'},
+  headers: {
+    'x-rapidapi-key': '939203f5d9msh8669d0a65c92f36p1977dfjsn088d46e4370a',
+    'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
+    useQueryString: true
+  }
+};
 
-req.query({
-	"source_id": "tv/63926",
-	"source": "tmdb",
-	"country": "us"
-});
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
 
-req.headers({
-	"x-rapidapi-key": "789443045cmshc58307b0d55a069p13f50cjsn36b252d16006",
-	"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-	"useQueryString": true
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
+	console.log(body);
 });
