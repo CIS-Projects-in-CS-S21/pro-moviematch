a1f379e8ca4ce3319cb7eb51f3670ae3c5ef122e
Brandon Lokey <57154350+gourmetpez@users.noreply.github.com>
2021-04-11 21:31:43 -0400

    Added TV genre IDs in comments for reference

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -27,6 +27,24 @@ import { Card } from '../components/Cards.js';
     Thriller        53
     War             10752
     Western         37
+
+  TMDB TV Genre IDs:
+    Action & Adventure  10759
+    Animation           16
+    Comedy              35
+    Crime               80
+    Documentary         99
+    Drama               18
+    Family              10751
+    Kids                10762
+    Mystery             9648
+    News                10763
+    Reality             10764
+    Sci-Fi & Fantasy    10765
+    Soap                10766
+    Talk                10767
+    War & Politics      10768
+    Western             37
 */
 
 export default function HomeScreen({ route, navigation }, page) {
