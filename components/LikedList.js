ed550834aa0a191387ba2afa3cec727aa60d8a49
Kyle Skelly <kyle.skelly32@gmail.com>
2021-04-20 17:15:29 -0400

    Poster images now showing + added ratings to list items

--- components/LikedList.js
+++ components/LikedList.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 //import {hasLoggedIn} from login;
 import login from '../server/validation/login';
 
-const tunnelURL = "https://bright-squid-11.loca.lt";
+const tunnelURL = "https://red-cobra-19.loca.lt";
 
 var movieidarray = [];
 
@@ -17,33 +17,6 @@ const Item = ({ title, caption}) => (
   </View>
 )
 
-const getUserID = async () =>{
-  try{
-    let response = await fetch(tunnelURL + "/api/users/" + global.globEmail + "/userId");
-    let jsonResponse = await response.json();
-    let userID = jsonResponse.userId;
-    global.userID = userID;
-  }
-  catch(error){
-    alert(error);
-  }
-};
-
-
-async function axiosTest() {
-  try {
-    const {data:response} = await axios.get(tunnelURL + '/api/users/'+ global.userID +'/like') //use data destructuring to get data from the promise object
-    console.log(response);
-    for (i = 0; i < response.length; i++) {
-      movieidarray.push(response[i]);
-    }
-    return parseMovies(response)
-  }
-
-  catch (error) {
-    console.log(error);
-  }
-}
 
 function parseMovies(movieArray) {
   var parsedMovies = [];
@@ -75,38 +48,33 @@ export default function LikedList({ navigation }) {
     try {
       axios.get(tunnelURL + '/api/users/'+ global.userID +'/like') //use data destructuring to get data from the promise object
       .then(function (response) {
-        console.log(response.data);
-    for (i = 0; i < response.data.length; i++) {
-      movieidarray.push(response.data[i]);
+        for (i = 0; i < response.data.length; i++) {
+          movieidarray.push(response.data[i]);
+        }
+    
+        //return parseMovies(response)
+        console.log("arraylength: " + movieidarray.length);
+        console.log("movieidarray: " + movieidarray[0]);
+        for (i = 0; i < movieidarray.length; i++) {
+          setLoading(true);
+          fetch("https://api.themoviedb.org/3/movie/" + movieidarray[i] + "?api_key=156f6cfa04dae615351cd9878f39b732")
+          .then((response2) => response2.json())
+          .then((responseJson) => {
+            //Successful response
+            //Increasing the offset for the next API call
+            setData(datas => [...datas, responseJson]);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        };
+      });
     }
-      //return parseMovies(response)
-      console.log("arraylength: " + movieidarray.length);
-      console.log("movieidarray: " + movieidarray[0]);
-      for (i = 0; i < movieidarray.length; i++) {
-        console.log("where am I : 2");
-        setLoading(true);
-        console.log(i);
-        fetch("https://api.themoviedb.org/3/movie/" + movieidarray[i] + "?api_key=156f6cfa04dae615351cd9878f39b732")
-        .then((response2) => response2.json())
-        .then((responseJson) => {
-          //Successful response
-          //Increasing the offset for the next API call
-          setData(datas => [...datas, responseJson]);
-          setLoading(false);
-          console.log("responseJson: " + responseJson);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      };
-    });
-  }
   
     catch (error) {
       console.log(error);
     }
-    console.log("where am I : 1");
-    console.log("i = " + i);
     
   }
   
@@ -115,7 +83,7 @@ export default function LikedList({ navigation }) {
     <ListItem bottomDivider>
       <Avatar 
         //size="large"
-        source={item.pic} 
+        source={{uri: "https://image.tmdb.org/t/p/original/" + item.poster_path}}
         width={100}
         height={150}
         imageProps={{resizeMode: 'contain'}}
@@ -123,6 +91,7 @@ export default function LikedList({ navigation }) {
       <ListItem.Content>
         <ListItem.Title>{item.title}</ListItem.Title>
         <ListItem.Subtitle>{item.overview}</ListItem.Subtitle>
+        <ListItem.Subtitle>{"Rating: "+item.vote_average}</ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
   );
